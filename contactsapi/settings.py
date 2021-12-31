989bbcbc4291560bbdbaac44b199bc9cbb5ed263
gitnoober <priyanshusingh349@gmail.com>
2022-01-01 00:16:48 +0530

    ff

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -170,6 +170,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
