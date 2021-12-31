322d0d78ca97994fb813dc1c96b9461503584ceb
gitnoober <priyanshusingh349@gmail.com>
2022-01-01 00:19:33 +0530

    ff

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -166,8 +166,9 @@ CORS_ALLOW_CREDENTIALS = True
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
