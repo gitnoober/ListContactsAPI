c2dc67a5d46cd8816dd4ba3596f7568b0b7c546a
gitnoober <priyanshusingh349@gmail.com>
2022-01-01 00:06:21 +0530

    ff

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -70,6 +70,7 @@ REST_FRAMEWORK={
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,6 +163,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = '/static/'
 
